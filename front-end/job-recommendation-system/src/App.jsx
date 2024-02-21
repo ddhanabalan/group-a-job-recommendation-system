de529951d098e1e64d666b15365889abec1236c1
Sreyas Kumar C V <sreyaskumar40@gmail.com>
2024-02-21 14:35:19 +0530

    sets vite+react environment and adds landing page
    adds material ui , react-bootstrap , local fonts

---
+++ front-end/job-recommendation-system/src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import LandingPage from './components/LandingPage';
+
+function App() {
+ 
+
+  return (
+    <>
+      <LandingPage/>
+    </>
+  )
+}
+
+export default App
