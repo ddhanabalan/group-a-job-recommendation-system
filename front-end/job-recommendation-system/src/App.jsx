c5866df155220608aa007e29079774d6c3868450
Sreyas Kumar C V <Sreyaskumar40@gmail.com>
2024-06-24 19:03:39 +0530

    fixes issues with auto refresh of access token

--- front-end/job-recommendation-system/src/App.jsx
+++ front-end/job-recommendation-system/src/App.jsx
@@ -24,27 +24,31 @@ import ReviewApplications from './pages/ReviewApplications/ReviewApplications';
 import OtherEmployerProfile from './pages/profile page/OtherEmployerProfile';
 import SeekerJobStatusSection from './pages/SeekerJobStatusSection/SeekerJobStatusSection';
 function App() {
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     if (getStorage("refToken")) {
-  //       refreshTokens()
-  //     }
-  //   }, 30000)//refresh token every 1 minute
-  // }, [])
-  // const refreshTokens = async () => {
-  //   try {
-  //     const response = await axios.get('/refresh_token', {
-  //       headers: {
-  //         'Authorization': `Bearer ${getStorage("refToken")}`
-  //       }
-  //     })
-  //     console.log(response)
-  //     response && setStorage("refToken", response.data.refresh_token)
-  //     response && setStorage("userToken", response.data.access_token)
-  //   } catch (e) {
-  //     console.log(e)
-  //   }
-  // }
+  useEffect(() => {
+    const refreshInterval = setInterval(() => {
+      if (getStorage("refToken")) {
+        refreshTokens()
+      }
+    }, 900000)//refresh token every 15 minutes
+
+    return () => clearInterval(refreshInterval)
+  }, [])
+  const refreshTokens = () => {
+
+    axios.get('/refresh_token', {
+      headers: {
+        'Authorization': `Bearer ${getStorage("refToken")}`
+      }
+   }).then((response) => {
+      console.log(response)
+      setStorage("refToken", response.data.refresh_token)
+      setStorage("userToken", response.data.access_token)
+    }).catch((error) => {
+      console.log(error)
+    })
+
+
+  }
   useEffect(() => {
     SetUser(getStorage("userType"))
   }, []);
@@ -76,7 +80,7 @@ function App() {
           <Route path="/verify/:accessToken" element={<VerifyAccount />} />
           {/* routes common to signed-in users */}
           <Route element={<PrivateRoutes />}>
-            <Route path="/profile" element={user !== null && user === "seeker" ? <ProfileSection data={{}} /> : user ==="recruiter" && <EmployerProfileSection data={{}} />} />
+            <Route path="/profile" element={user !== null && user === "seeker" ? <ProfileSection data={{}} /> : user === "recruiter" && <EmployerProfileSection data={{}} />} />
             <Route path="/profile/:username" element={<OtherUserProfile data={{}} />} />
             <Route path="/e/profile/:username" element={<OtherEmployerProfile data={{}} />} />
             <Route path="/employer-profile" element={<EmployerProfileSection data={{}} />} />
@@ -84,8 +88,8 @@ function App() {
           {/* routes exclusive to seekers */}
           <Route element={<SeekerRoutes />}>
             <Route path="/jobs" element={<JobSection />} />
-            <Route path="/seeker/openings" element={<ReviewApplications userType="seeker"/>} />
-            <Route path="/seeker/applications" element={<SeekerJobStatusSection userType="seeker"/>} />
+            <Route path="/seeker/openings" element={<ReviewApplications userType="seeker" />} />
+            <Route path="/seeker/applications" element={<SeekerJobStatusSection userType="seeker" />} />
           </Route>
           {/* routes exclusive to recruiters */}
           <Route element={<EmployerRoutes />}>
