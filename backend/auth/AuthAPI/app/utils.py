0461e3b8ee8fe2672738255d0e39d7e8c0c4fb67
Sreeraj S <sreerajs1063@gmail.com>
2024-07-19 16:54:20 +0000

    Changes Done For Faster deployment

--- backend/auth/AuthAPI/app/utils.py
+++ backend/auth/AuthAPI/app/utils.py
@@ -36,7 +36,7 @@ valid for 30 minutes. If the link expires, please log in again to generate a new
         If you believe you received this email in error or did not initiate this request, please don't hesitate to \
 contact us.
         
-        http://localhost:5173/verify/{token}
+        http://career-go.centralindia.cloudapp.azure.com/verify/{token}
         
         Thank you for your attention to this matter.
         
@@ -74,7 +74,7 @@ valid for 30 minutes. If the link expires, please log in again to generate a new
         If you believe you received this email in error or did not initiate this request, please don't hesitate to \
 contact us.
         
-        http://localhost:5173/forgot_password/verify/{token}
+        http://career-go.centralindia.cloudapp.azure.com/forgot_password/verify/{token}
         
         Thank you for your attention to this matter.
         
