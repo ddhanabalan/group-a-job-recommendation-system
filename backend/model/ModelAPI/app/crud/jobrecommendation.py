9886d81b1ec7fc91eeb5490f824e49bea293128a
Sreeraj S <sathishranji1@gmail.com>
2024-07-19 22:20:37 +0530

    Updated Model Further Optimization

--- backend/model/ModelAPI/app/crud/jobrecommendation.py
+++ backend/model/ModelAPI/app/crud/jobrecommendation.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from typing import List, Type, Optional
 
-from ..models import model
+from ..models import model, JobRecommendationJobOutput
 
 
 def create_job_input(
@@ -28,3 +28,16 @@ def create_seeker_input(
     except SQLAlchemyError as e:
         db.rollback()
         return False
+
+
+def get_applicant(db: Session, applicant_id: int) -> list[model.JobRecommendationJobOutput] | []:
+    try:
+        return db.query(model.JobRecommendationJobOutput).filter(model.JobRecommendationJobOutput.user_id == applicant_id).all()
+    except SQLAlchemyError as e:
+        return []
+
+def get_job(db: Session, job_pos: str) -> list[model.JobRecommendationSeekerOutput] | []:
+    try:
+        return db.query(model.JobRecommendationSeekerOutput).filter(model.JobRecommendationSeekerOutput.job_position == job_pos).all()
+    except SQLAlchemyError as e:
+        return []
