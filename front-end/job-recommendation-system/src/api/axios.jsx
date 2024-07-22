7cf008438356497904c43330381b51e504ddbbe6
Sreyas Kumar C V <sreyaskumar40@gmail.com>
2024-07-22 20:10:53 +0530

    fixes css logic in candidate card

--- front-end/job-recommendation-system/src/api/axios.jsx
+++ front-end/job-recommendation-system/src/api/axios.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-//let ip="192.168.1.8";
-let ip="localhost";
-// let ip="career-go.centralindia.cloudapp.azure.com"
+// let ip="192.168.1.6";
+// let ip="localhost";
+let ip="career-go.centralindia.cloudapp.azure.com"
 
 export const authAPI =axios.create({
      baseURL: `http://${ip}:8000/`
