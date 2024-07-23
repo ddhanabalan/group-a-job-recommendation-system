f21e27f96d673927661f859f3f3ab1c5fb4bcf18
njo-117 <anjotjuby@gmail.com>
2024-07-23 17:48:54 +0530

    Fixes bugs in Review Applications Sections for Recruiters

--- front-end/job-recommendation-system/src/components/FeatureBoxMiddlePane/FeatureBoxMiddlePaneOpenings.jsx
+++ front-end/job-recommendation-system/src/components/FeatureBoxMiddlePane/FeatureBoxMiddlePaneOpenings.jsx
@@ -3,6 +3,7 @@ import JobCard from '../JobCard/JobCard';
 import { Button } from '@mui/material'
 import { Link } from 'react-router-dom';
 import {getStorage} from '../../storage/storage';
+import { v4 as uuid } from 'uuid';
 import './FeatureBoxMiddlePane.css';
 export default function FeatureBoxMiddlePaneOpenings({data, childData}) {
     const demoInfo = { jobTitle: "Python Developer", companyName: "Google LLC",workStyle:"on-site",workingDays:"Monday-Friday", skills: ["python","javascript"], currency: "₹", salary: "50k", postDate: "13/9/23" };
@@ -18,7 +19,7 @@ export default function FeatureBoxMiddlePaneOpenings({data, childData}) {
             <div className="feature-box-container">
             {Object.keys(companyOpenings).length?
             <>
-            {Object.keys(companyOpenings).map((card) => (<JobCard key={companyOpenings[card]["id"]} id={companyOpenings[card]["id"]}  data={{ ...companyOpenings[card], 'userType': "seeker" }} link={data.userType == "employer"?"/employer/review-applications": `/seeker/openings/${getStorage("guestUserID")}/${companyOpenings[card]["id"]}`} />)).slice(0,2)}
+            {Object.keys(companyOpenings).map((card) => (<JobCard key={uuid()} id={companyOpenings[card]["id"]}  data={{ ...companyOpenings[card], 'userType': "seeker" }} link={data.userType == "employer"?"/employer/review-applications": `/seeker/openings/${getStorage("guestUserID")}/${companyOpenings[card]["id"]}`} />)).slice(0,2)}
             <hr className="line-separator"/>
                 <div className='see-openings-footer'>
                     <Link to={data.userType == "employer"?"/employer/review-applications": `/seeker/openings/${getStorage("guestUserID")}/all`} state={{user_id: data.user_id}}>
