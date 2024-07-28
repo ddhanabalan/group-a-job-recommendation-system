45ae9d6b584aa9ceed5bdccedb83ae5680eed48a
Sreyas Kumar C V <Sreyaskumar40@gmail.com>
2024-07-28 22:49:34 +0530

    fixes css in details card

--- front-end/job-recommendation-system/src/components/DetailsCard/DetailsCard.jsx
+++ front-end/job-recommendation-system/src/components/DetailsCard/DetailsCard.jsx
@@ -17,23 +17,23 @@ import AddLocationAltRoundedIcon from '@mui/icons-material/AddLocationAltRounded
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 import PublicIcon from '@mui/icons-material/Public';
 import { utilsAPI } from '../../api/axios';
-import {Autocomplete} from '@mui/material';
+import { Autocomplete } from '@mui/material';
 import './DetailsCard.css';
 // import '../FeatureBox/FeatureBox.css';
 // import './ContactCard.css';
-export default function ContactCard({access, data, companyInfo, reloadFn, showSuccessMsg, showFailMsg }) {
+export default function ContactCard({ access, data, companyInfo, reloadFn, showSuccessMsg, showFailMsg }) {
     const RETRY_DELAY = 500;
     const [isNotEditing, SetIsNotEditing] = useState(true);
     const { register, formState: { errors }, getValues, trigger, setError, setValue } = useForm({});
 
     const backupCountries = [{ "country": 'India' }, { "country": 'USA' }, { "country": "Germany" }, { "country": 'Australia' }, { 'country': "Japan" }]
     const [countries, setCountries] = useState([])
-    
+
     const [fetchingErrors, setFetchingErrors] = useState({ "countries": false });
     const [userHeadquarters, setUserHeadquarters] = useState({ "country": data.headquarters, 'industry': data.industry })
     const backupIndustries = [{ "industry": 'Automobile' }, { "industry": 'Agriculture' }, { "industry": 'Medical' }, { "industry": 'Defense' }, { "industry": 'Aeronautical' }, { "industry": 'Chemical' }]
     const [industries, setIndustries] = useState([])
-    const [userIndustry, setUserIndustry] = useState({industry: companyInfo.industry})
+    const [userIndustry, setUserIndustry] = useState({ industry: companyInfo.industry || "" })
 
     const fetchCountries = async () => {
         try {
@@ -60,23 +60,23 @@ export default function ContactCard({access, data, companyInfo, reloadFn, showSu
 
     const fetchIndustries = async () => {
         try {
-          const r = await utilsAPI.get('api/v1/industry/');
-          if (r.data.length) {
-            setIndustries(r.data);
-            setFetchingErrors({ ...fetchingErrors, "industries": false });
-          }
-          else {
-            setIndustries(backupIndustries);
-            setFetchingErrors({ ...fetchingErrors, "industries": true });
-          }
+            const r = await utilsAPI.get('api/v1/industry/');
+            if (r.data.length) {
+                setIndustries(r.data);
+                setFetchingErrors({ ...fetchingErrors, "industries": false });
+            }
+            else {
+                setIndustries(backupIndustries);
+                setFetchingErrors({ ...fetchingErrors, "industries": true });
+            }
         }
         catch (e) {
-          console.log("industry fetch failed", e);
-          //alert("industries not fetched");
-          setIndustries(backupIndustries);
-          setFetchingErrors({ ...fetchingErrors, "industries": true });
+            console.log("industry fetch failed", e);
+            //alert("industries not fetched");
+            setIndustries(backupIndustries);
+            setFetchingErrors({ ...fetchingErrors, "industries": true });
         }
-      }
+    }
 
     async function updateContact(data) {
         SetIsNotEditing(true)
@@ -109,14 +109,14 @@ export default function ContactCard({access, data, companyInfo, reloadFn, showSu
     }
     const parallelRetryFn = (delay) => {
         setTimeout(() => {
-          if (fetchingErrors.industries) fetchIndustries();
-          if (fetchingErrors.countries) fetchCountries();
+            if (fetchingErrors.industries) fetchIndustries();
+            if (fetchingErrors.countries) fetchCountries();
         }, delay);
         console.log(delay, " ms over")
         return () => clearInterval(parallelRetryFn);
-      }
+    }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchCountries()
         fetchIndustries()
         setValue('headquarters', data.headquarters)
@@ -124,14 +124,14 @@ export default function ContactCard({access, data, companyInfo, reloadFn, showSu
     useEffect(() => {
         console.log("fetching errors", fetchingErrors, industries, countries)
         if (Object.keys(fetchingErrors).some((e) => fetchingErrors[e] === true)) parallelRetryFn(RETRY_DELAY);
-    
-      }, [fetchingErrors])
+
+    }, [fetchingErrors])
 
     return (
         <form className="feature-box detail-box" >
             < h4 className="feature-title" > {data.title}</h4 >
             <Stack direction="row" spacing={0} className='feature-actions'>
-                {access!=="viewOnly"&& (isNotEditing ? [data.editIcon] &&
+                {access !== "viewOnly" && (isNotEditing ? [data.editIcon] &&
                     <IconButton aria-label="edit" onClick={() => SetIsNotEditing(false)}>
                         <EditIcon />
                     </IconButton> :
@@ -200,38 +200,51 @@ export default function ContactCard({access, data, companyInfo, reloadFn, showSu
                     :
 
                     <Stack direction="column" spacing={1} className='contact-cards'>
-                        <Stack direction="row" spacing={1} className='detail-medium'>
+                        <Stack direction="column" spacing={1} className='detail-medium' sx={{alignItems: "flex-start"}}>
                             <div className='detail-identifier'>
                                 <IconButton aria-label="industry" disabled>
                                     <FactoryRoundedIcon />
                                 </IconButton>
                                 <p className='stat-title'>Industry</p>
                             </div>
-                            
+
                             <Autocomplete
                                 disablePortal
+                                className='details-card-industry'
                                 options={industries}
                                 placeholder='Automobile'
                                 value={userIndustry}
                                 getOptionLabel={(option) => option["industry"]}
+                                componentsProps={{
+                                    popper: {
+                                        modifiers: [
+                                            {
+                                                name: 'flip',
+                                                enabled: false
+                                            }
+                                        ]
+                                    }
+                                }}
                                 isOptionEqualToValue={(option) => industries.some(e => e["industry"] === option)}
                                 onChange={(event, newInputValue) => {
                                     setUserIndustry(newInputValue)
                                     setValue('industry', newInputValue["industry"])
                                 }}
-                                renderInput={(params) => 
-                                <TextField
-                                className="personal-details-input profile-edit-bio contact-card-textfield"
-                                {...params}
-                                InputProps={{
-                                    ...params.InputProps,
-                                    disableUnderline: true,
-                                }}
+                                renderInput={(params) =>
+                                    <TextField
+                                        // className="personal-details-input profile-edit-bio contact-card-textfield"
+                                        {...params}
+                                        placeholder='Automobile'
+                                        sx={{ padding: '.5rem'}}
+                                        InputProps={{
+                                            ...params.InputProps,
+                                            disableUnderline: true,
+                                        }}
 
-                                variant="outlined"
+                                        variant="outlined"
 
-                                {...register("industry", { required: "Field is required", })}
-                                />}
+                                        {...register("industry", { required: "Field is required", })}
+                                    />}
                             />
                         </Stack>
 
